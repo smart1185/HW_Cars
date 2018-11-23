38b50d48fd7a3b9cf390da12857a5800c8fe90db
HP <HP@USER>
2018-11-23 15:09:20 +0600

    Добавьте файлы проекта.

---
+++ HW_3/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using HW_3.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public static Frame mf { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            mf = MainFrame;
+        }
+        private void MainFrame_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow.mf.Source = new Uri("Pages/Page1.xaml", UriKind.RelativeOrAbsolute);
+            Page1 page1 = new Page1();
+            MainWindow.mf.NavigationService.Navigate(page1);
+        }
+    }
+}
---
+++ HW_3/Pages/Page2.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_3.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }        
+        private void tbxVIN_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tbxVIN.Text = null;
+            tbxVIN.Foreground = new SolidColorBrush(Colors.Black);
+        }
+
+        private void tbx1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tbx1.Text = null;
+            tbx1.Foreground = new SolidColorBrush(Colors.Black);
+        }
+
+        private void tbx2_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tbx2.Text = null;
+            tbx2.Foreground = new SolidColorBrush(Colors.Black);
+        }
+    }
+}
